1be195e65a0db5de1c80adeafa3f052675535a9b
srimal_n <srimal@pickme.lk>
2022-08-16 01:09:40 +0530

    make keys component a functional component

--- ui/src/components/keys.js
+++ ui/src/components/keys.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 
 import FSNavigator from './tree';
 import Grid from '@mui/material/Grid';
@@ -6,129 +6,94 @@ import Paper from '@mui/material/Paper';
 import EditorComponent from './editor';
 import DataService from '../data/service'
 
-class KeysComponent extends React.Component {
-    dataService;
-    constructor() {
-        super()
-        this.state = {
-            activeKey: "/",
-            keys: {
-                id: 'root',
-                name: 'Parent',
-            },
-            isNewKey: false
-        }
+export default function Keys(props) {
+    const dataService = useMemo( () => new DataService(), []);
 
-        this.setActiveKey = this.setActiveKey.bind(this);
-        this.fetchKeys = this.fetchKeys.bind(this);
-        this.createVirtualDirectory = this.createVirtualDirectory.bind(this);
-        this.createVirtualFile = this.createVirtualFile.bind(this);
+    const [activeKey, setActiveKey] = useState("/");
+    const [keys, setKeys] = useState({ id: 'root', name: 'Parent' });
+    const [isNewKey, setIsNewKey] = useState(false);
 
-        this.dataService = new DataService();
-    }
-
-    setActiveKey(key) {
-        this.setState({
-            activeKey: key
-        })
-    }
-
-    createVirtualFile = async (path) => {
+    const createVirtualFile = async (path) => {
         try {
-            let fileTree = await this.dataService.CreateNode(path, false);
-            this.setState({
-                keys: fileTree,
-                activeKey: path,
-                isNewKey: true
-            });
+            let fileTree = await dataService.CreateNode(path, false);
+            setKeys(fileTree);
+            setActiveKey(path);
+            setIsNewKey(true);
         } catch (error) {
             //TODO: display an error message
             console.error(error);
         }
     }
 
-    createVirtualDirectory = async (path) => {
+    const createVirtualDirectory = async (path) => {
         try {
-            let fileTree = await this.dataService.CreateNode(path, true);
-            this.setState({
-                keys: fileTree
-            });
+            let fileTree = await dataService.CreateNode(path, true);
+            setKeys(fileTree);
         } catch (error) {
             //TODO: display an error message
             console.error(error);
         }
     }
 
-    setupNewKey(path) {
-        this.setState({
-            activeKey: path,
-            isNewKey: true
-        })
-    }
-
-    deleteKey = async (node) => {
+    const deleteKey = async (node) => {
         try {
-            let isSuccess = await this.dataService.DeleteNode(node.abspath, node.type === "directory");
+            let isSuccess = await dataService.DeleteNode(node.abspath, node.type === "directory");
             if (isSuccess) {
-                this.fetchKeys();
+                fetchKeys();
             }
         } catch (error) {
             console.error(error);
         }
     }
 
-    fetchKeys = async () => {
+    const fetchKeys = useCallback(async () => {
         try {
-            let keys = await this.dataService.GetKeys();
-            this.setState({ keys: keys });
+            let keys = await dataService.GetKeys();
+            setKeys(keys);
         } catch (error) {
             // TODO: display an error
             console.error(error);
         }
-    }
+    }, [dataService])
 
-    componentDidMount() {
-        this.fetchKeys();
-    }
+    useEffect(() => {
+        fetchKeys();
+    }, [fetchKeys])
 
-    render() {
-        return (
-            <React.Fragment>
-                <Grid container spacing={3}>
+    return (
+        <React.Fragment>
+            <Grid container spacing={3}>
 
-                    <Grid item xs={12} md={4} lg={3}>
-                        <Paper
-                            sx={{
-                                p: 2,
-                                display: 'flex',
-                                flexDirection: 'column',
-                                height: 800,
-                            }}
-                        >
-                            <FSNavigator
-                                keys={this.state.keys}
-                                onKeyClick={this.setActiveKey}
-                                fetchKeys={this.fetchKeys}
-                                createFile={this.createVirtualFile}
-                                createDirectory={this.createVirtualDirectory}
-                                deleteKey={this.deleteKey}
-                            />
-                        </Paper>
-                    </Grid>
-
-                    <Grid item xs={12} md={8} lg={9}>
-                        <EditorComponent etcdKey={this.state.activeKey} isNewKey={this.state.isNewKey} />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', height: "40px" }}>
-                            {/* <Orders /> */}
-                        </Paper>
-                    </Grid>
+                <Grid item xs={12} md={4} lg={3}>
+                    <Paper
+                        sx={{
+                            p: 2,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            height: 800,
+                        }}
+                    >
+                        <FSNavigator
+                            keys={keys}
+                            onKeyClick={setActiveKey}
+                            fetchKeys={fetchKeys}
+                            createFile={createVirtualFile}
+                            createDirectory={createVirtualDirectory}
+                            deleteKey={deleteKey}
+                        />
+                    </Paper>
                 </Grid>
-            </React.Fragment>
-        )
-    }
-}
 
-export default KeysComponent;
+                <Grid item xs={12} md={8} lg={9}>
+                    <EditorComponent etcdKey={activeKey} isNewKey={isNewKey} />
+                </Grid>
+
+                <Grid item xs={12}>
+                    <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', height: "40px" }}>
+                        {/* <Orders /> */}
+                    </Paper>
+                </Grid>
+            </Grid>
+        </React.Fragment>
+    )
+}
